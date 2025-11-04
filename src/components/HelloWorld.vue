efe85b089214495b6a88aa4bc041a31b4699ba84
JaraVerbrugghe <jara.verbrugghe@student.howest.be>
2025-11-04 15:10:11 +0100

    install theme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p class="text-3xl">
+    <p class="text-3xl text-alphaRed font-secondary">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
