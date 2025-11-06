a94cad33a3ffbe6f7ede7cc7e8134e666cf1f74e
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-11-06 13:24:58 +0100

    detail page responsive

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
   
   <div class="h-screen overflow-y-scroll overflow-x-hidden">
     <!-- <HomePage></HomePage> -->
-     <AllProjects></AllProjects>
-     <!-- <DetailProject></DetailProject> -->
+     <!-- <AllProjects></AllProjects> -->
+     <DetailProject></DetailProject>
   </div>
 </template>
 
 <script setup lang="ts"> // @ts-ignore 
 // import HomePage from './screens/HomePage.vue' // @ts-ignore 
-import AllProjects from './screens/AllProjects.vue' // @ts-ignore 
-// import DetailProject from './screens/DetailProject.vue' // @ts-ignore 
+// import AllProjects from './screens/AllProjects.vue' // @ts-ignore 
+import DetailProject from './screens/DetailProject.vue' // @ts-ignore 
 </script>
