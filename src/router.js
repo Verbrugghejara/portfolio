fd9a9281b2fa9b49768ec83465378f2a83c8f52f
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-11-10 22:40:20 +0100

    fix refresh page

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from './screens/HomePage.vue'
 import Projects from './screens/AllProjects.vue'
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/portfolio/'),
+  history: createWebHashHistory('/portfolio/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // Als er een opgeslagen positie is (zoals bij router.back()), ga daarheen
